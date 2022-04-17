96c8b0024435937b7615d560099e9a0570a885f2
emerson.da.s.barros <kaua54@hotmail.com>
2022-04-16 22:35:52 -0300

    iniciando testes de integração

--- src/test/java/com/barrostech/AlgafoodApiApplicationTests.java
+++ src/test/java/com/barrostech/AlgafoodApiApplicationTests.java
@@ -1,13 +1,60 @@
 package com.barrostech;
 
+import com.barrostech.domain.exception.EntidadeEmUsoException;
+import com.barrostech.domain.model.Cozinha;
+import com.barrostech.domain.services.CadastroCozinhaService;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
 
 	@Test
-	void contextLoads() {
+	public void testarCadastroCozinhaComNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+
+	@Test
+	public void testarCadastroCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+
+		ConstraintViolationException erroEsperado =
+				Assertions.assertThrows(ConstraintViolationException.class, () -> {
+					cadastroCozinha.salvar(novaCozinha);
+				});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+
+	@Test
+	public void testarFalhaExcluirCozinhaEmUso(){
+		Cozinha cozinha = new Cozinha();
+		cozinha = cadastroCozinha.buscarOuFalhar(1L);
+
+		Cozinha finalCozinha = cozinha;
+		EntidadeEmUsoException erroEsperado =
+				Assertions.assertThrows(EntidadeEmUsoException.class, ()->{
+					cadastroCozinha.excluir(finalCozinha.getId());
+				});
+
+
 	}
 
 }
